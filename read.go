caeb4a354204c690c23047951c553b480b000296
nopdan <xci@live.com>
2021-11-30 22:44:09 +0800

    fix read bug

--- read.go
+++ read.go
@@ -15,11 +15,11 @@ func ReadFile(f io.Reader) (io.Reader, error) {
 	detector := chardet.NewTextDetector()
 	cs, err := detector.DetectBest(buf) // 检测编码格式
 	if err != nil {
-		return f, err
+		return brd, err
 	}
 	if cs.Confidence != 100 && cs.Charset != "UTF-8" {
 		cs.Charset = "GB18030"
 	}
-	rd, err := charset.NewReaderLabel(cs.Charset, f) // 转换字节流
+	rd, err := charset.NewReaderLabel(cs.Charset, brd) // 转换字节流
 	return rd, err
 }

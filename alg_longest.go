6d1e2d08ba9b9903c5b8c6f7857e186e7a817425
nopdan <xci@live.com>
2022-04-29 16:58:30 +0800

    优化 longest

--- alg_longest.go
+++ alg_longest.go
@@ -7,48 +7,37 @@ type code_order struct {
 
 // 最长匹配
 type longest struct {
-	max int
-	m   map[int]map[string]code_order
+	l []map[string]code_order
 }
 
 func NewLongest() *longest {
-	ret := new(longest)
-	ret.m = make(map[int]map[string]code_order)
-	return ret
+	l := new(longest)
+	l.l = make([]map[string]code_order, 0, 30)
+	return l
 }
 
 func (l *longest) Insert(word, code string, order int) {
-	length := len([]rune(word))
-	if l.m[length] == nil {
-		l.m[length] = make(map[string]code_order)
+	i := len([]rune(word)) // 词长
+	for i+1 > len(l.l) {   // 扩容
+		l.l = append(l.l, make(map[string]code_order))
 	}
 	// 不替换原有的
-	if l.m[length][word].code == "" {
-		l.m[length][word] = code_order{code, order}
+	if l.l[i][word].code == "" {
+		l.l[i][word] = code_order{code, order}
 	}
 }
 
 func (l *longest) Handle() {
-	for k := range l.m {
-		if k > l.max {
-			l.max = k
-		}
-	}
-	for i := 1; i < l.max; i++ {
-		if l.m[i] == nil {
-			l.m[i] = make(map[string]code_order)
-		}
-	}
 }
 
 // 最长匹配
 func (l *longest) Match(text []rune, p int) (int, string, int) {
-	max := l.max
+	max := len(l.l) - 1
 	if len(text)-p < max {
 		max = len(text) - p
 	}
 	for i := max; i > 0; i-- {
-		v, ok := l.m[i][string(text[p:p+i])]
+		v, ok := l.l[i][string(text[p:p+i])]
 		if ok {
 			return i, v.code, v.order
 		}

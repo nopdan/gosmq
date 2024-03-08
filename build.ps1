4ede12b033dc54a227bf39a5a3966495c0fe7589
nopdan <xci@live.com>
2024-03-08 15:36:14 +0800

    chore: use bun.js

--- build.ps1
+++ build.ps1
@@ -1,15 +1,11 @@
 
-cd ../web
-pnpm i
-pnpm build
+cd frontend
+bun install
+bun run build
 cd ..
-xcopy web\dist\ internal\serve\dist\ /E /Y
+xcopy frontend\dist\ pkg\server\dist\ /E /Y
 
 go mod tidy
 
 Write-Output "编译 windows 版本"
 go build -ldflags="-s -w" -o build/smq.exe
-
-cd build
-
-upx .\smq.exe
